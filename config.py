7bd122e6625a986876ecba07b441e07296456916
Kritika-Dhiman <144471110+Kritika-Dhiman@users.noreply.github.com>
2025-03-28 06:03:02 +0530

    Added config.py and secured environment variables

---
+++ config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+class Config(object):
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY is not set. Please configure it in .env")
+
+load_dotenv()  # Load environment variables from .env
+
+secret_key = os.getenv("SECRET_KEY")
+database_url = os.getenv("DATABASE_URL")
+
+print("Secret Key:", secret_key)  # Just to check if it's working
